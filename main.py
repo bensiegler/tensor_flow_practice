e11dcf99f32df581194d254db23ae171fbf624fc
benjaminsiegler <bensiegler@icloud.com>
2023-09-11 00:00:28 +0200

    added optimization and regularization to model

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-import tensorflow
+import keras.src.regularizers
+import tensorflow as tf
 from tensorflow.python.keras import Sequential
-from tensorflow.python.keras.layers import Dense
+from tensorflow.python.keras.layers import Dense, Conv1D
+from tensorflow.python.keras.regularizers import L2
 
 model = Sequential([
-    Dense(units=25, activation='relu'),
-    Dense(units=15, activation='relu'),
-    Dense(units=10, activation='linear')
+    Dense(units=25, activation='relu', kernel_regularizer=L2(0.02)),
+    Dense(units=15, activation='relu', kernel_regularizer=L2(0.02)),
+    Dense(units=10, activation='linear', kernel_regularizer=L2(0.02))
 ])
 
-from tensorflow.python.keras.losses import SparseCategoricalCrossentropy
 
-model.compile(loss=SparseCategoricalCrossentropy(from_logits=True))
+model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True))
 
 # model.fit(X, Y, )
