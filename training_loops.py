6f5580d2e06deb3acc8fa1493ad4f45e962d79f0
benjaminsiegler <bensiegler@icloud.com>
2023-09-18 23:20:28 +0200

    added optimization and regularization to model

---
+++ training_loops.py
@@ -0,0 +1,21 @@
+import matplotlib
+from matplotlib import pyplot as plt
+import tensorflow as tf
+
+matplotlib.rcParams['figure.figsize'] = [9, 6]
+
+x = tf.linspace(-2, 2, 201)
+x = tf.cast(x, tf.float32)
+
+
+def f(x):
+    y = x ** 2 + 2 * x - 5
+    return y
+
+
+y = f(x) + tf.random.normal(shape=[201])
+
+plt.plot(x.numpy(), y.numpy(), '.', label='Data')
+plt.plot(x, f(x), label='Ground truth')
+plt.legend()
+plt.show()
