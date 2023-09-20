abaac2496c38924abc5673f60a1f65ee0e7759fe
benjaminsiegler <bensiegler@icloud.com>
2023-09-20 23:23:12 +0200

    some more keras practice

--- training_loops.py
+++ training_loops.py
@@ -1,6 +1,12 @@
 import matplotlib
 from matplotlib import pyplot as plt
 import tensorflow as tf
+from tensorflow import keras
+
+from keras import Sequential
+from keras.layers import Dense, Conv1D
+from keras.regularizers import L2
+from keras.losses import MeanSquaredError
 
 matplotlib.rcParams['figure.figsize'] = [9, 6]
 
@@ -17,5 +23,25 @@ y = f(x) + tf.random.normal(shape=[201])
 
 plt.plot(x.numpy(), y.numpy(), '.', label='Data')
 plt.plot(x, f(x), label='Ground truth')
+
+model = Sequential([
+    Dense(units=25, activation='relu'),
+    Dense(units=20, activation='relu'),
+    Dense(units=1, activation='linear')
+])
+
+x = tf.convert_to_tensor(x)
+y = tf.convert_to_tensor(y)
+
+print(x.shape)
+print(y.shape)
+model.compile(loss=MeanSquaredError(), optimizer='adam')
+
+model.fit(tf.expand_dims(x, axis=-1), y, epochs=5000)
+
+predictions = model.predict(x)
+
+plt.plot(x, predictions, label='prediction')
 plt.legend()
 plt.show()
+
